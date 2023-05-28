556267e750fd92987a7d76869f5b8a4347a4d7b0
Sixtus Agbo <miracleagbosixtus@gmail.com>
2023-05-28 10:36:36 +0100

    Get user input

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,15 @@
-console.log('Hello World!');
+const form = document.querySelector('form') as HTMLFormElement;
+const addressInput = document.getElementById('address') as HTMLInputElement;
+
+function searchPlaceHandler(event: SubmitEvent) {
+  event.preventDefault();
+  // get user input
+  const enteredAddress = addressInput.value;
+  console.log(enteredAddress);
+
+  // look up place with Google Map API
+
+  // render the map to the DOM
+}
+
+form.addEventListener('submit', searchPlaceHandler);
