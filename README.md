5aec18632a56d6fe009577466837719057f54170
Sixtus Agbo <miracleagbosixtus@gmail.com>
2023-05-28 08:21:56 +0100

    Apparently I did something

---
+++ README.md
@@ -0,0 +1,3 @@
+# Place Explorer
+
+Simple, minimal application you can use to find your place on the world map🗺️. I just made this project to practice TypeScript. Huge thanks to [Google Maps](https://developers.google.com/maps), their API made it possible.
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const CleanPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/app.ts',
+  output: {
+    filename: 'app.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: './',
+  },
+  devServer: {
+    static: './',
+    compress: true,
+    host: '0.0.0.0',
+    port: 8080,
+    devMiddleware: {
+      index: true,
+      mimeTypes: { phtml: 'text/html' },
+      publicPath: './',
+      serverSideRender: true,
+      writeToDisk: true,
+    },
+  },
+  devtool: 'eval-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  plugins: [new CleanPlugin.CleanWebpackPlugin()],
+};
